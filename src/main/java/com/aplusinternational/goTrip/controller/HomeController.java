6b5bada31e4887d8131a020c241b4a44db7b3181
doyeon <doyeon326@gmail.com>
2021-02-25 13:50:41 +0900

    image uploadd

--- src/main/java/com/aplusinternational/goTrip/controller/HomeController.java
+++ src/main/java/com/aplusinternational/goTrip/controller/HomeController.java
@@ -1,35 +1,46 @@
 package com.aplusinternational.goTrip.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
+import java.io.IOException;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.aplusinternational.goTrip.service.S3Service;
+
+import lombok.AllArgsConstructor;
 
 /**
  * Handles requests for the application home page.
  */
+@AllArgsConstructor
 @Controller
 public class HomeController {
 	
 	@Autowired
 	S3Service s3_service;
-
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		System.out.println("in");
+//		System.out.println("in");
 		s3_service.setS3Client();
-		System.out.println("out");
-		
+	
 		return "home";
+	}
+	
+	@PostMapping("/upload")
+	@ResponseBody
+	public String upload (@RequestParam("data") MultipartFile multipartFile) throws IOException {
+	
+		return s3_service.upload(multipartFile, "static");
+		
 	}
 	
 }
--- src/main/java/com/aplusinternational/goTrip/service/S3Service.java
+++ src/main/java/com/aplusinternational/goTrip/service/S3Service.java
@@ -1,38 +1,65 @@
 package com.aplusinternational.goTrip.service;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import java.io.IOException;
 
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.Protocol;
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
-import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import lombok.NoArgsConstructor;
+
 
 @Service
+@NoArgsConstructor
 public class S3Service {
-	private AmazonS3 s3Client;
-	
-	@Value("${cloud.aws.credentials.accessKey}")
+    private AmazonS3 s3Client;
+    
+
+    @Value("${cloud.aws.credentials.accessKey}")
     private String accessKey;
 
     @Value("${cloud.aws.credentials.secretKey}")
     private String secretKey;
 
-    @Value("${cloud.aws.s3.bucket}")
+    @Value("#{systemProperties['cloud.aws.s3.bucket']}")
     private String bucket;
 
     @Value("${cloud.aws.region.static}")
     private String region;
-	
-	public void setS3Client() {
-		System.out.println("in");
-		System.out.println("name of the bucket:" + bucket);
-		//AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+
+    @PostConstruct
+    public void setS3Client() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        s3Client = AmazonS3ClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+                .withRegion(this.region)
+                .build();
+    }
+
+	public String upload(MultipartFile multipartFile, String dirName) throws IOException {
+		String fileName = dirName + "/" + multipartFile.getOriginalFilename();
+		String uploadImageUrl = putS3(multipartFile, fileName);
+		//removeNewFile(multipartFile);
+		return uploadImageUrl;
 	}
-	
+
+	private String putS3(MultipartFile multipartFile, String fileName) throws IOException {
+		System.out.println("file is in!: "+ fileName+" putS3");
+		System.out.println(this.bucket);
+		s3Client.putObject(new PutObjectRequest(bucket, fileName, multipartFile.getInputStream(), null).withCannedAcl(CannedAccessControlList.PublicRead));
+		return s3Client.getUrl(bucket, fileName).toString();
+	}
+
+
 }
